bfd3071c8d7346d42ddffea3ab217a1ce65fdd0d
Sunil Thapa <sunilthapa056@outlook.com>
2025-07-22 12:56:51 +0545

    working with dao

--- src/controller/ViewController.java
+++ src/controller/ViewController.java
@@ -2,14 +2,14 @@ package controller;
 
 import model.BillRecords;
 import view.CaculateBillView;
+import view.HighestPayingView;
 
 public class ViewController {
     public static void getCalculateBillView() {
         CaculateBillView.showCalculateBillView();
     }
 
-
     public static void getHighestPayingView() {
-
+        HighestPayingView.showHighestPayingView();
     }
 }
